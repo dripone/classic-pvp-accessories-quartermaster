e7ca2f93081402456966637bc6751531fbc39267
dripone <166992353+dripone@users.noreply.github.com>
2024-08-03 14:01:00 +0200

    Add files via upload

---
+++ classic_pvp_vendors.sql
@@ -0,0 +1,47 @@
+DELETE FROM `npc_vendor` WHERE `item` IN (
+    25829, -- Talisman of the Alliance
+    24551, -- Talisman of the Horde
+    28118, -- Runed Ornate Ruby
+    28119, -- Smooth Ornate Dawnstone
+    28120, -- Gleaming Ornate Dawnstone
+    28123, -- Potent Ornate Topaz
+    28234, -- Medallion of the Alliance Rogue
+    28235, -- Medallion of the Alliance Druid
+    28236, -- Medallion of the Alliance Paladin
+    28237, -- Medallion of the Alliance Hunter
+    28238, -- Medallion of the Alliance Mage
+    28239, -- Medallion of the Horde Mage
+    28240, -- Medallion of the Horde Rogue
+    28241, -- Medallion of the Horde Druid
+    28242, -- Medallion of the Horde Paladin
+    28243, -- Medallion of the Horde Hunter
+    37864, -- Medallion of the Alliance ( EPIC )
+	37865, -- Medallion of the Horde ( EPIC ) 
+    30348, -- Medallion of the Alliance Warlock
+    30349, -- Medallion of the Alliance Priest
+    30350, -- Medallion of the Alliance Warrior
+    30351, -- Medallion of the Alliance Shaman
+    30343, -- Medallion of the Alliance Warlock
+    30346, -- Medallion of the Alliance Priest
+    30344, -- Medallion of the Alliance Warrior
+    30345, -- Medallion of the Alliance Shaman
+    28246, -- Band of Triumph
+    28247, -- Band of Dominance
+    28362, -- Bold Ornate Ruby
+    28363, -- Inscribed Ornate Topaz
+    28379, -- Sergeant's Heavy Cape
+    28377, -- Sergeant's Heavy Cloak
+    28378, -- Sergeant's Heavy Cape
+    28380, -- Sergeant's Heavy Cloak
+    31838, -- Major Combat Healing Potion
+    31839, -- Major Combat Healing Potion
+    31840, -- Major Combat Mana Potion
+    31841, -- Major Combat Mana Potion
+    31852, -- Major Combat Healing Potion
+    31853, -- Major Combat Healing Potion
+    31854, -- Major Combat Mana Potion
+    31855, -- Major Combat Mana Potion
+    32453, -- Major Combat Mana Potion
+    32455, -- Major Combat Mana Potion
+    44957 -- Greater Inscription of the Gladiator
+);
